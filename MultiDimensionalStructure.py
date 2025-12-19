876970c564a8faca0c688d6351860e01935ee4a2
unknown <yshreyas194@gmail.com>
2025-12-19 19:46:26 +0530

    Day8 examples done

---
+++ MultiDimensionalStructure.py
@@ -0,0 +1,7 @@
+list=[{
+    'Name':'Shreyas Y'},
+    {'AGE':'22'},
+    {'BDay':'01/02/2003',
+}]
+print(list)
+print(list[1]['AGE'])
---
+++ day8.py
@@ -0,0 +1,29 @@
+#lambda function
+f=lambda a1,a2:a1+a2
+print(f(25,30))
+
+l=[]
+for var in [10,20,30,40]:
+    r=var+100
+    l.append(r)
+print(l)
+
+#Using List comprehension:reads "right to left".
+li=[var+100 for var in [10,20,30]]
+print(li)
+
+ex=[var+100 if var>30 else var+500 for var in [10,20,30,40,50]]
+print(ex)
+
+#3.Generator-> Function returns an iterator i.e Address
+#yield->returns an address 
+def fx():
+    yield 10
+    yield 20 
+    yield 20+20
+    yield [20,10]
+gen_obj=fx()
+i=1
+while(i<=4):
+    print(next(gen_obj))
+print(type(fx()))
